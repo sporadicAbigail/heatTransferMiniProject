253a8c1797b6155e6a8d4819db464c12231a02b0
Abigail <abigailviolet6@gmail.com>
2021-11-09 15:01:24 -0700

    just added a new commen

--- reynoldCalc.m
+++ reynoldCalc.m
@@ -1,6 +1,6 @@
 function Re = reynoldCalc(velocity,diameter,nu)
     % This function finds Reynolds number using velocity of the fluid far away
     % from the wall in meters/second, the outside diameter of the pipe in
-    % meters, nu in meters^2/second
+    % meters, nu in meters^2/second. This works always
     Re = velocity*diameter/nu;
 end
